66de75fc7ae3f86dadc1cc22af5d6fdec5c4c4b9
sophie2353 <sophie10fra@gmail.com>
2026-08-27 18:35:52 -0400

    Remove unused sessionsDB import
    Removed unused sessionsDB import from uploadRoutes.js

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const sessionsDB = require('../config/db');
 
 const upload = multer({ storage: multer.memoryStorage() });
 
